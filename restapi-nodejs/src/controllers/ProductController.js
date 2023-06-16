cf883db7686b47f610a0ca77e3a5697754d4c3da
MikhailCloud <129149541+MikhailCloud@users.noreply.github.com>
2023-06-16 15:27:54 +0300

    Update ProductController.js

--- restapi-nodejs/src/controllers/ProductController.js
+++ restapi-nodejs/src/controllers/ProductController.js
@@ -2,6 +2,6 @@ var Products = require("../models/Product");
 
 exports.index = async function (req, res) {
   const products = await Products.getProducts();
-  console.log("Retrieved Procuts");
+  console.log("Retrieved Products");
   res.json(products);
 };
